abea23bdd073ee563fd0a6f07a59b98c4e099344
Rachait Talwar <rachaittalwar159@gmail.com>
2026-06-08 21:21:50 +0530

    new code

---
+++ largestelemet.java
@@ -0,0 +1,21 @@
+import java .util.Scanner;
+public class largestelemet {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int n = sc.nextInt();
+        
+        int [] arr = new int[n];
+        int largest = arr[0];
+        System.out.println("Enter the elements of array");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i] > largest){
+                largest = arr[i];
+            }
+        }
+        System.out.println("largest elemtent is " +largest);
+    }
+}
--- tuf.java
+++ tuf.java
@@ -403,30 +403,40 @@ class Main {
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
 import java.util.Scanner;
-class Main {
+
+public class largestelemet {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of array");
         int n = sc.nextInt();
-        
+
         int[] arr = new int[n];
-        int largest = arr[0];
-        int second = -1;
-        for(int i=0;i<n;i++){
+
+        System.out.println("Enter the elements of array");
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        // for(int i=0; i<n; i++){
-        //     if(arr[i] > largest){
-        //         largest = arr[i];
-        //     }
-        // }
-        // System.out.println(largest);
-        for(int i = n-2; i>=0; i--){
-            if(arr[i] != largest){
-                second = arr[i];
-                break;
+
+        int largest = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
             }
         }
-        System.out.println(second);
+        System.out.println("First "+largest);
+        int second = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != largest) {
+                if (second == -1 || arr[i] > second) {
+                    second = arr[i];
+                }
+            }
+        }
+
+        System.out.println("Second " + second);
     }
 }
 
